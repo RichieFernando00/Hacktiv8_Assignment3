e9d36f0543a4b9866ea843c44b1101cf74109426
RichieFernando00 <89447425+RichieFernando00@users.noreply.github.com>
2021-10-25 14:56:10 +0700

    Add files via upload

---
+++ ASSIGNMENT 3/tugas1.js
@@ -0,0 +1,12 @@
+let myRecipe =  ["In a large bowl, beat the eggs. In another bowl, combine the salt, pepper, garlic powder and paprika to make a seasoned salt. Add the flour to a third bowl.",
+        "Prepare the chicken by first seasoning each piece of chicken to your taste with the seasoned salt. Then dip each piece in the egg and drag through flour until coated well.",
+        "Fill a large pot or deep fryer half full with oil and heat to 350°F. Place the chicken parts in and fry until dark and crisp. The thighs and legs will take longer to cook – about 15 minutes – than the breast and wings, which will take about 10 minutes.",
+        "Remove from heat, let sit for 5 minutes to cool and enjoy!"];
+
+console.log('How To Make Fried Chicken')
+
+console.log('Ingridients : ')
+console.log("1 Chicken","\n","3 Egg","\n","2 cup Flour","\n","1 Tbsp Kosher Salt","\n","2 tsp Pepper","\n","1 tsp Garlic Powder","\n","1 tsp Paprika","\n","Vegetable Oil");
+
+console.log('How To Make : ')
+console.log({ 'Step 1': myRecipe[0] }, '\n', { 'Step 2': myRecipe[1] }, '\n', { 'Step 3': myRecipe[2] }, '\n', { 'Step 4': myRecipe[3] })
---
+++ ASSIGNMENT 3/tugas2.js
@@ -0,0 +1,22 @@
+let favMeals =  ["Fried Rice","Fried Potato,","Burger","Chips","Boba Tea"];
+
+console.log('My Favorite Food & Drinks')
+
+console.log('Sebelum pop, shift, unshift, push : ')
+console.log(favMeals)
+
+console.log('Setelah Push')
+favMeals.push("Thai Tea");
+console.log(favMeals)
+
+console.log('Setelah Unshift')
+favMeals.unshift("Sausage","Yogurth");
+console.log(favMeals)
+
+console.log('Setelah Shift')
+favMeals.shift();
+console.log(favMeals)
+
+console.log('Setelah Pop')
+favMeals.pop();
+console.log(favMeals)
